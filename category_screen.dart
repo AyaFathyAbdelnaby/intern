cff4e798416eda50de2be835007311020d05f41f
AyaFathyAbdelnaby <139823268+AyaFathyAbdelnaby@users.noreply.github.com>
2023-07-18 19:03:46 +0300

    Update category_screen.dart

--- category_screen.dart
+++ category_screen.dart
@@ -1,8 +1,26 @@
+import 'package:flutter/material.dart';
+
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      //appBar:AppBar(title: Text("data")) ,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 36,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 22),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Icon(Icons.arrow_back), Icon(Icons.favorite_border)],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
