6e8a30cb383342e4635a90665dbe30db8000f5c3
AyaFathyAbdelnaby <139823268+AyaFathyAbdelnaby@users.noreply.github.com>
2023-07-19 20:01:47 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:pr_ovider/counter_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider.value(value: CounterProvider())],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late CounterProvider counterprovider;
+  @override
+  void initState() {
+    counterprovider = Provider.of(context, listen: false);
+    super.initState();
+  }
+
+  void _incrementCounter() {
+    counterprovider.incrment();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Consumer<CounterProvider>(
+              builder: (context, provider, child) {
+                return Text(
+                  '${provider.counter}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
